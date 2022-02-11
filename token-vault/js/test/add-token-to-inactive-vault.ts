0e6f60908c22834686f819a9af23824515e521db
Thorsten Lorenz <thlorenz@gmx.de>
2022-02-11 15:59:06 -0600

    test: adding multi add token tests

--- token-vault/js/test/add-token-to-inactive-vault.ts
+++ token-vault/js/test/add-token-to-inactive-vault.ts
@@ -8,7 +8,7 @@ import {
   killStuckProcess,
   spokSamePubkey,
 } from './utils';
-import { Transaction } from '@solana/web3.js';
+import { Transaction, TransactionInstruction } from '@solana/web3.js';
 import { assertConfirmedTransaction, assertTransactionSummary } from '@metaplex-foundation/amman';
 import {
   addTokenToInactiveVault,
@@ -85,3 +85,104 @@ test('inactive vault: add tokens once', async (t) => {
     tokenTypeCount: 1,
   });
 });
+
+test('inactive vault: add tokens thrice via three different safety deposit boxes', async (t) => {
+  const {
+    transactionHandler,
+    connection,
+    accounts: initVaultAccounts,
+  } = await initVault(t, { allowFurtherShareCreation: true });
+  const { payer, vault, authority: vaultAuthority, vaultAuthorityPair } = initVaultAccounts;
+
+  async function verifyAccountStates(
+    safetyDepositSetup: SafetyDepositSetup,
+    tokenTypeCount: number,
+  ) {
+    const { safetyDeposit, tokenMint, store } = safetyDepositSetup;
+    const safetyDepositAccountInfo = await connection.getAccountInfo(safetyDeposit);
+    assertIsNotNull(t, safetyDepositAccountInfo);
+    const [safetyDepositAccount] = SafetyDepositBox.fromAccountInfo(safetyDepositAccountInfo);
+
+    spok(t, safetyDepositAccount, {
+      $topic: 'safetyDepositAccount',
+      key: Key.SafetyDepositBoxV1,
+      vault: spokSamePubkey(vault),
+      tokenMint: spokSamePubkey(tokenMint),
+      store: spokSamePubkey(store),
+    });
+
+    await assertInactiveVault(t, connection, initVaultAccounts, {
+      allowFurtherShareCreation: true,
+      tokenTypeCount,
+    });
+  }
+
+  async function submitAndVerifyTransaction(
+    safetyDepositSetup: SafetyDepositSetup,
+    addTokenIx: TransactionInstruction,
+  ) {
+    const tx = new Transaction().add(...safetyDepositSetup.instructions).add(addTokenIx);
+    const signers = [...safetyDepositSetup.signers, vaultAuthorityPair];
+
+    const res = await transactionHandler.sendAndConfirmTransaction(tx, signers);
+    assertConfirmedTransaction(t, res.txConfirmed);
+    assertTransactionSummary(t, res.txSummary, {
+      msgRx: [
+        /InitializeMint/i,
+        /Transfer \d+ lamports to.+ associated token account/i,
+        /Approve/i,
+        /Add token to vault/i,
+        /Transfer/i,
+      ],
+    });
+  }
+
+  {
+    const TOKEN_AMOUNT = 2;
+    t.comment(`++++ 1. Adding tokens with amount ${TOKEN_AMOUNT}`);
+
+    const safetyDepositSetup = await SafetyDepositSetup.create(connection, {
+      payer,
+      vault,
+      mintAmount: TOKEN_AMOUNT,
+    });
+
+    const addTokenIx = await addTokenToInactiveVault(safetyDepositSetup, { payer, vaultAuthority });
+    await submitAndVerifyTransaction(safetyDepositSetup, addTokenIx);
+    await verifyAccountStates(safetyDepositSetup, 1);
+  }
+
+  {
+    const TOKEN_AMOUNT = 3333;
+    t.comment(`++++ 2. Adding tokens with amount ${TOKEN_AMOUNT}`);
+
+    const safetyDepositSetup = await SafetyDepositSetup.create(connection, {
+      payer,
+      vault,
+      mintAmount: TOKEN_AMOUNT,
+    });
+
+    const addTokenIx = await addTokenToInactiveVault(safetyDepositSetup, { payer, vaultAuthority });
+    await submitAndVerifyTransaction(safetyDepositSetup, addTokenIx);
+    await verifyAccountStates(safetyDepositSetup, 2);
+  }
+
+  {
+    const TOKEN_AMOUNT = 3333;
+    t.comment(`++++ 3. Adding tokens with amount ${TOKEN_AMOUNT}`);
+
+    const safetyDepositSetup = await SafetyDepositSetup.create(connection, {
+      payer,
+      vault,
+      mintAmount: TOKEN_AMOUNT,
+    });
+
+    const addTokenIx = await addTokenToInactiveVault(safetyDepositSetup, { payer, vaultAuthority });
+    await submitAndVerifyTransaction(safetyDepositSetup, addTokenIx);
+    await verifyAccountStates(safetyDepositSetup, 3);
+  }
+});
+
+// TODO(thlorenz): Here we only include the _happy path_ tests not even covering all possibilities
+// More tests should be added especially the ones confirming that incorrectly
+// setup accounts are handled correctly by returning an error from the program.
